a7c31c07fa7d07338e9803fffa02cb2a897b09d3
Apes <53538501+SolaApes@users.noreply.github.com>
2019-08-05 13:41:12 -0700

    Update rng.lua

--- lua/rng.lua
+++ lua/rng.lua
@@ -2,29 +2,12 @@ function get_sets()
 
 	-- Preshot --
     sets.Preshot = {
-		head="Orion beret +3",
-		neck="Scout's Gorget +2",
-        body="Amini caban +1",
-		hands="Carmine Fin. Ga. +1",
-        back={ name="Belenus's Cape", augments={'"Snapshot"+10',}},
-		waist="Yemaya Belt",
-		legs="Adhemar Kecks +1",
+
 	}
 				
 	 -- Midshot --
     sets.Midshot = {
-		head="Arcadian Beret +2",
-		neck="Scout's Gorget +2",
-		ear1="Dedition Earring",
-		ear2="Enervating Earring",
-		body="Orion Jerkin +3",
-        hands="Adhemar Wrist. +1",
-		ring1="Petrov ring",
-		ring2="Rajas ring",
-        back={ name="Belenus's Cape", augments={'AGI+20','Rng.Acc.+20 Rng.Atk.+20','AGI+10','"Store TP"+10',}},
-		waist="Yemaya belt",
-		legs="Adhemar Kecks +1",
-		feet="Adhe. Gamashes +1",
+
 	}
 
 end
